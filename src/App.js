2f7b592c3b598a436b6f5ece2c773f0b96298cdd
Monicaasensio <Monicaasensio@yahoo.com>
2024-01-05 11:32:59 -0500

    completed easy challenge

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 import React, { Component } from 'react'
 
 export default class App extends Component {
+constructor(props) {
+  super(props);
+
+  this.state = {
+    person: {}
+  }
+}
+
   render() {
     return (
       <div>
